2e96693ad0e8a1dd1959b3a7ffb66c78bf412bbd
Martin Gajdos <martingajdos.zs@gmail.com>
2020-11-09 09:26:15 +0100

    edit model for db POST

--- ActivityBlog/resources/views/index.blade.php
+++ ActivityBlog/resources/views/index.blade.php
@@ -2,160 +2,176 @@
 <html lang="en">
 
 <head>
-  <meta charset="UTF-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-  <title>Project Board</title>
-  <link rel="stylesheet" href="<?php echo asset('css/app.css')?>" type="text/css">
-  <link rel="stylesheet" href="<?php echo asset('css/all.min.css')?>" type="text/css">
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
-    integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
-    integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
-    crossorigin="anonymous"></script>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx"
-    crossorigin="anonymous"></script>
+    <meta charset="UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <meta http-equiv="X-UA-Compatible" content="ie=edge"/>
+    <title>Project Board</title>
+    <link rel="stylesheet" href="<?php echo asset('css/app.css')?>" type="text/css">
+    <link rel="stylesheet" href="<?php echo asset('css/all.min.css')?>" type="text/css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
+          integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+            integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
+            crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx"
+            crossorigin="anonymous"></script>
 
 
-   <!-- <script src="scripts/app.js" defer></script> -->
+    <!-- <script src="scripts/app.js" defer></script> -->
 </head>
 
 <body>
 
-  <!-- Dalsi postup a tooltipy-->
-  <template id="tooltip">
+<!-- Dalsi postup a tooltipy-->
+<template id="tooltip">
     <h2>More Info</h2>
     <p></p>
-  </template>
-  <header id="main-header">
+</template>
+<header id="main-header">
 
     <nav class="navbar navbar-expand-lg navbar-light  justify-content-between ">
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
-        aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class=" logo" >
-        <a class="navbar-brand" href="#"></a>
-      </div>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
+                aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class=" logo">
+            <a class="navbar-brand" href="#"></a>
+        </div>
 
 
-      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-        <ul class="navbar-nav">
-          <li class="nav-item active">
-            <a class="nav-link" href="index.html"> <svg width="1em" height="1em" viewBox="0 0 16 16"
-                class="bi bi-house-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                <path fill-rule="evenodd"
-                  d="M8 3.293l6 6V13.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5V9.293l6-6zm5-.793V6l-2-2V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5z" />
-                <path fill-rule="evenodd"
-                  d="M7.293 1.5a1 1 0 0 1 1.414 0l6.647 6.646a.5.5 0 0 1-.708.708L8 2.207 1.354 8.854a.5.5 0 1 1-.708-.708L7.293 1.5z" />
-              </svg> Home <span class="sr-only">(current)</span></a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="creation"> <svg width="1em" height="1em" viewBox="0 0 16 16"
-                class="bi bi-pencil" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                <path fill-rule="evenodd"
-                  d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5L13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175l-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z" />
-              </svg>Creation</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="events"> <svg width="1em" height="1em" viewBox="0 0 16 16"
-                class="bi bi-calendar-event-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                <path fill-rule="evenodd"
-                  d="M4 .5a.5.5 0 0 0-1 0V1H2a2 2 0 0 0-2 2v1h16V3a2 2 0 0 0-2-2h-1V.5a.5.5 0 0 0-1 0V1H4V.5zM16 14V5H0v9a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2zm-3.5-7a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1z" />
-              </svg> Events</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link " href="eshop"> <svg width="1em" height="1em" viewBox="0 0 16 16"
-                class="bi bi-shop" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                <path fill-rule="evenodd"
-                  d="M2.97 1.35A1 1 0 0 1 3.73 1h8.54a1 1 0 0 1 .76.35l2.609 3.044A1.5 1.5 0 0 1 16 5.37v.255a2.375 2.375 0 0 1-4.25 1.458A2.371 2.371 0 0 1 9.875 8 2.37 2.37 0 0 1 8 7.083 2.37 2.37 0 0 1 6.125 8a2.37 2.37 0 0 1-1.875-.917A2.375 2.375 0 0 1 0 5.625V5.37a1.5 1.5 0 0 1 .361-.976l2.61-3.045zm1.78 4.275a1.375 1.375 0 0 0 2.75 0 .5.5 0 0 1 1 0 1.375 1.375 0 0 0 2.75 0 .5.5 0 0 1 1 0 1.375 1.375 0 1 0 2.75 0V5.37a.5.5 0 0 0-.12-.325L12.27 2H3.73L1.12 5.045A.5.5 0 0 0 1 5.37v.255a1.375 1.375 0 0 0 2.75 0 .5.5 0 0 1 1 0zM1.5 8.5A.5.5 0 0 1 2 9v6h1v-5a1 1 0 0 1 1-1h3a1 1 0 0 1 1 1v5h6V9a.5.5 0 0 1 1 0v6h.5a.5.5 0 0 1 0 1H.5a.5.5 0 0 1 0-1H1V9a.5.5 0 0 1 .5-.5zM4 15h3v-5H4v5zm5-5a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1h-2a1 1 0 0 1-1-1v-3zm3 0h-2v3h2v-3z" />
-              </svg> Shop</a>
-          </li>
-        </ul>
-      </div>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <ul class="navbar-nav">
+                <li class="nav-item active">
+                    <a class="nav-link" href="index.html">
+                        <svg width="1em" height="1em" viewBox="0 0 16 16"
+                             class="bi bi-house-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd"
+                                  d="M8 3.293l6 6V13.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5V9.293l6-6zm5-.793V6l-2-2V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5z"/>
+                            <path fill-rule="evenodd"
+                                  d="M7.293 1.5a1 1 0 0 1 1.414 0l6.647 6.646a.5.5 0 0 1-.708.708L8 2.207 1.354 8.854a.5.5 0 1 1-.708-.708L7.293 1.5z"/>
+                        </svg>
+                        Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="creation">
+                        <svg width="1em" height="1em" viewBox="0 0 16 16"
+                             class="bi bi-pencil" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd"
+                                  d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5L13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175l-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"/>
+                        </svg>
+                        Creation</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="events">
+                        <svg width="1em" height="1em" viewBox="0 0 16 16"
+                             class="bi bi-calendar-event-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd"
+                                  d="M4 .5a.5.5 0 0 0-1 0V1H2a2 2 0 0 0-2 2v1h16V3a2 2 0 0 0-2-2h-1V.5a.5.5 0 0 0-1 0V1H4V.5zM16 14V5H0v9a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2zm-3.5-7a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1z"/>
+                        </svg>
+                        Events</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link " href="eshop">
+                        <svg width="1em" height="1em" viewBox="0 0 16 16"
+                             class="bi bi-shop" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd"
+                                  d="M2.97 1.35A1 1 0 0 1 3.73 1h8.54a1 1 0 0 1 .76.35l2.609 3.044A1.5 1.5 0 0 1 16 5.37v.255a2.375 2.375 0 0 1-4.25 1.458A2.371 2.371 0 0 1 9.875 8 2.37 2.37 0 0 1 8 7.083 2.37 2.37 0 0 1 6.125 8a2.37 2.37 0 0 1-1.875-.917A2.375 2.375 0 0 1 0 5.625V5.37a1.5 1.5 0 0 1 .361-.976l2.61-3.045zm1.78 4.275a1.375 1.375 0 0 0 2.75 0 .5.5 0 0 1 1 0 1.375 1.375 0 0 0 2.75 0 .5.5 0 0 1 1 0 1.375 1.375 0 1 0 2.75 0V5.37a.5.5 0 0 0-.12-.325L12.27 2H3.73L1.12 5.045A.5.5 0 0 0 1 5.37v.255a1.375 1.375 0 0 0 2.75 0 .5.5 0 0 1 1 0zM1.5 8.5A.5.5 0 0 1 2 9v6h1v-5a1 1 0 0 1 1-1h3a1 1 0 0 1 1 1v5h6V9a.5.5 0 0 1 1 0v6h.5a.5.5 0 0 1 0 1H.5a.5.5 0 0 1 0-1H1V9a.5.5 0 0 1 .5-.5zM4 15h3v-5H4v5zm5-5a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1h-2a1 1 0 0 1-1-1v-3zm3 0h-2v3h2v-3z"/>
+                        </svg>
+                        Shop</a>
+                </li>
+            </ul>
+        </div>
 
     </nav>
 
-  </header>
-   <!-- Hlavny obsah-->
-  <div class="container">
+</header>
+<!-- Hlavny obsah-->
+
+
+<div class="container">
     <div class="row">
-      <ul class="info rounded list-group col-md-2">
-        <li class="list-group-item d-flex justify-content-between align-items-center ">Active: <span
-            class="badge badge-primary badge-pill">2</span> </li>
-        <li class="list-group-item d-flex justify-content-between align-items-center">Finished: <span
-            class="badge badge-primary badge-pill">1</span> </li>
-        <li class="list-group-item d-flex justify-content-between align-items-center">Main Goal: <span
-            class="badge badge-primary badge-pill"><svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-check"
-              fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+        <!-- Statistika-->
+        <ul class="info rounded list-group col-md-2">
+            <li class="list-group-item d-flex justify-content-between align-items-center ">Active: <span
+                    class="badge badge-primary badge-pill">2</span></li>
+            <li class="list-group-item d-flex justify-content-between align-items-center">Finished: <span
+                    class="badge badge-primary badge-pill">1</span></li>
+            <li class="list-group-item d-flex justify-content-between align-items-center">Main Goal: <span
+                    class="badge badge-primary badge-pill"><svg width="1em" height="1em" viewBox="0 0 16 16"
+                                                                class="bi bi-check"
+                                                                fill="currentColor" xmlns="http://www.w3.org/2000/svg">
               <path fill-rule="evenodd"
-                d="M10.97 4.97a.75.75 0 0 1 1.071 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.236.236 0 0 1 .02-.022z" />
-            </svg></span> </li>
-      </ul>
+                    d="M10.97 4.97a.75.75 0 0 1 1.071 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.236.236 0 0 1 .02-.022z"/>
+            </svg></span></li>
+        </ul>
+        <!-- Konci panel Statistika-->
 
-      <div class="col-md-10">
+        <div class="col-md-10">
 
-        <section id="active-projects">
-          <header>
-            <h2>Active Projects</h2>
-          </header>
-          <ul>
-            <li id="p1" data-extra-info="Got lifetime access, but would be nice to finish it soon!" class="card"
-              draggable="true">
-              <h2>Finish a exercise2 from VAII</h2>
-              <p>Finish the exercise2 within the next two weeks.</p>
-              <button class="alt">More Info</button>
-              <button>Finish</button>
-            </li>
-            <li id="p2" data-extra-info="Not really a business topic but still important." class="card"
-              draggable="true">
-              <!--  casom to chcem aj aby sa to dalo drag Event -->
-              <h2>Buy Groceries</h2>
-              <p>Don't forget to pick up groceries today.</p>
-              <button class="alt">More Info</button>
-              <button>Finish</button>
-            </li>
-          </ul>
-        </section>
-        <section id="finished-projects">
-          <header>
-            <h2>Finished Projects</h2>
-          </header>
-          <ul>
-            <li id="p3" data-extra-info="Super important party! Needed to be cool!" class="card" draggable="true">
-              <h2> <span> <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-exclamation-square text-danger"
-                    fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+            <section id="active-projects">
+                <header>
+                    <h2>Active Projects</h2>
+                </header>
+                <ul>
+                    <li id="p1" data-extra-info="Got lifetime access, but would be nice to finish it soon!" class="card"
+                        draggable="true">
+                        <h2>Finish a exercise2 from VAII</h2>
+                        <p>Finish the exercise2 within the next two weeks.</p>
+                        <button class="alt">More Info</button>
+                        <button>Finish</button>
+                    </li>
+                    <li id="p2" data-extra-info="Not really a business topic but still important." class="card"
+                        draggable="true">
+                        <!--  casom to chcem aj aby sa to dalo drag Event -->
+                        <h2>Buy Groceries</h2>
+                        <p>Don't forget to pick up groceries today.</p>
+                        <button class="alt">More Info</button>
+                        <button>Finish</button>
+                    </li>
+                </ul>
+            </section>
+            <section id="finished-projects">
+                <header>
+                    <h2>Finished Projects</h2>
+                </header>
+                <ul>
+                    <li id="p3" data-extra-info="Super important party! Needed to be cool!" class="card"
+                        draggable="true">
+                        <h2> <span> <svg width="1em" height="1em" viewBox="0 0 16 16"
+                                         class="bi bi-exclamation-square text-danger"
+                                         fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                     <path fill-rule="evenodd"
-                      d="M14 1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z" />
+                          d="M14 1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
                     <path
-                      d="M7.002 11a1 1 0 1 1 2 0 1 1 0 0 1-2 0zM7.1 4.995a.905.905 0 1 1 1.8 0l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 4.995z" />
-                  </svg></span> Book Table </h2>
-              <p>
-                My birthday party takes place in November, don't forget to book a
-                table in restaurant.
-              </p>
-              <button class="alt">More Info</button>
-              <button>Activate</button>
-            </li>
-          </ul>
-        </section>
-      </div>
-    </div>
-  </div>
-  <!--Pata-->
-  <footer class="site-footer">
-    <div class="container">
-      <div class="row">
-        <div class="col align-self-center">
-          <p class="copyright-text">Copyright &copy; 2020 All Rights Reserved by
-            Gajdy
-          </p>
+                        d="M7.002 11a1 1 0 1 1 2 0 1 1 0 0 1-2 0zM7.1 4.995a.905.905 0 1 1 1.8 0l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 4.995z"/>
+                  </svg></span> Book Table
+                        </h2>
+                        <p>
+                            My birthday party takes place in November, don't forget to book a
+                            table in restaurant.
+                        </p>
+                        <button class="alt">More Info</button>
+                        <button>Activate</button>
+                    </li>
+                </ul>
+            </section>
         </div>
-      </div>
-      </div>
-      <!-- <button id="stop-analytics-btn">Stop Analytics</button> -->
-  </footer>
+    </div>
+</div>
+<!--Pata-->
+<footer class="site-footer">
+    <div class="container">
+        <div class="row">
+            <div class="col align-self-center">
+                <p class="copyright-text">Copyright &copy; 2020 All Rights Reserved by
+                    Gajdy
+                </p>
+            </div>
+        </div>
+    </div>
+    <!-- <button id="stop-analytics-btn">Stop Analytics</button> -->
+</footer>
 </body>
 
 </html>
--- ActivityBlog/resources/views/pages/creation.blade.php
+++ ActivityBlog/resources/views/pages/creation.blade.php
@@ -13,7 +13,7 @@
   <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
     integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
     crossorigin="anonymous"></script>
-    
+
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx"
     crossorigin="anonymous" defer></script>
@@ -93,6 +93,8 @@
             <span aria-hidden="true">&times;</span>
           </button>
         </div>
+
+          <!-- Modalne okno na pridanie zaznamu-->
         <div class="modal-body">
           <form>
             <div class="form-row">
@@ -103,13 +105,13 @@
                 <input type="text" class="form-control" placeholder="Title of the activity">
               </div>
 
-              
+
                 <div class="col-md-2">
                   <label  class="titleTag">Categ.: </label>
                 </div>
                 <div class="col-md-2">
-              
-               
+
+
                 <select class="custom-select" id="validationTooltip04" required>
                   <option selected disabled value="">Choose...</option>
                   <option value="1">Sport</option>
