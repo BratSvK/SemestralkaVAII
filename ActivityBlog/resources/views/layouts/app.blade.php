93114ce1f68311ed84859dd0fb4736eb211d18fc
Martin Gajdos <martingajdos.zs@gmail.com>
2020-12-13 12:06:47 +0100

    edit login form view

--- ActivityBlog/resources/views/layouts/app.blade.php
+++ ActivityBlog/resources/views/layouts/app.blade.php
@@ -5,17 +5,13 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <meta name="theme-color" content="#7952b3">
     <title>Project Board</title>
-    <link rel="stylesheet" href="<?php echo asset('css/app.css')?>" type="text/css">
-    <link rel="stylesheet" href="<?php echo asset('css/all.min.css')?>" type="text/css">
-    <link rel="stylesheet" href="<?php echo asset('css/events.css')?>" type="text/css">
+    <link rel="stylesheet" href="<?php echo asset('css/login.css')?>" type="text/css">
 
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
           integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-
-    <script defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyA_XutZaq6kFoCFVsGC1TCwWCHZmz1VLII">
-    </script>
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
             integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
     </script>
@@ -24,31 +20,12 @@
     </script>
 
 
-    <script type="module" src="{{ URL::asset('js/events.js') }}" defer></script>
-    <!-- <script src="assets/scripts/app.js" defer></script> zatial len zalozenie hlavicky pre JavaScript defer sluzi na vykonanie po zobrazeni HTML stránky -->
 </head>
 
-<body>
-<div class="container">
-
-    @yield("content")
-</div>
+<body class="text-center">
+@yield('content')
 
 
-
-<!--Pata-->
-<footer class="site-footer">
-    <div class="container">
-        <div class="row">
-            <div class="col align-self-center">
-                <p class="copyright-text">Copyright &copy; 2020 All Rights Reserved by
-                    Gajdy
-                </p>
-            </div>
-        </div>
-    </div>
-    <!-- <button id="stop-analytics-btn">Stop Analytics</button> -->
-</footer>
 </body>
 
 </html>
